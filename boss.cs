5ad33f1cf59b1f38837c449519e2fe1d7da704f2
luiz guilherme bezerra da silva <98851888+luizguibe@users.noreply.github.com>
2022-03-02 22:49:33 -0300

    Add files via upload
    atualizado com o  pulo constante.

--- boss.cs
+++ boss.cs
@@ -8,6 +8,10 @@ public class inifollow : MonoBehaviour
     public float speed;
     private Transform Target; //alvo
      public float StoppingDistance; // nao coloque uma distancia muito alta.
+      private Transform GroundCheck;
+     private BoxCollider2D colisor;
+     private float JumpForce;
+     private RigidBody2D rig;
 
    
 
@@ -19,8 +23,7 @@ public class inifollow : MonoBehaviour
     {
         Target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         //coloque dentro das aspas o  player que o inimigo quer seguir.Eh so ir em inspector ,tag  e configurar como player.
-
-
+        rig = GetComponent<RigidBody2D>();
 
     }
 
@@ -30,6 +33,15 @@ public class inifollow : MonoBehaviour
         if(Vector2.Distance(transform.position, Target.position) > StoppingDistance){
             transform.position = Vector2.MoveTowards(transform.position, Target.position, speed*Time.deltaTime);
         }
+        if(Grounded()){
+            rig.velocity = Vector2.Up*JumpForce;
 
+        }
+
+    }
+      bool Grounded()
+    {
+        RaycastHit2D ground = Physics2D.Raycast(colisor.bounds.center, colisor.bounds.size, 0 ,Vector2.down, 0.1f);
+        return ground.collider != null;
     }
 }
